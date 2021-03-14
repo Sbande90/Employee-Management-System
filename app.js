198b5374d8088ebfbee082427d1d7dae2893d641
salfoband90@gmail.com <salfoband90@gmail.com>
2021-03-14 01:21:25 -0500

    added view.js

--- app.js
+++ app.js
@@ -1,34 +1,56 @@
 
 const inq = require("inquirer");
-const table = require("console.table");
+// const table = require("console.table");
 const add = require("./lib/add");
 const update = require("./lib/update");
 const view = require("./lib/view");
+const addDep = require("./lib/addDepartment");
+const addRole = require("./lib/addRole");
 
-const app = { 
-start: () => {
+
+exports.start = () => {
     inq.prompt([
         {
-            type: "list",
+            type: "rawlist",
             message: "what would you like to do?",
             name: "menu",
             choices: [
                 "View All Employees",
+                "View Departments",
+                "View Roles",
                 "Add Employee",
+                "Add Department",
+                "Add Role",
                 "Update Employee Role",
                 "Exit"
             ]
         }
     ])
-    .then(async function(answer){
+    .then(function(answer){
         console.log(answer);
         if(answer.menu === "View All Employees"){
-            view.viewAllEmployee();
+            view.showEmployees();
+        }
+        else if(answer.menu === "View Departments"){
+            view.showDepartment();
+        }
+        else if(answer.menu === "View Roles"){
+            view.showRoles();
         }
         else if(answer.menu === "Add Employee"){
-            console.log("here");
-           await add.addEmployee();
-           app.start();
+            // console.log("here");
+            add.addEmployee();
+           
+        }
+        else if(answer.menu === "Add Department"){
+            // console.log("here");
+            addDep.addD();
+           
+        }
+        else if(answer.menu === "Add Role"){
+            // console.log("here");
+            addRole.addR();
+           
         }
         else if(answer.menu === "Update Employee Role"){
             update.updateRole();
@@ -38,6 +60,5 @@ start: () => {
             return
         }
     });
-}};
-app.start();
-module.exports = app;
+};
+
